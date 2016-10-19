8cf971b5fcd94921472d400bdceb7feb0d1d5446
Anshuman Suri <anshuman14021@iiitd.ac.in>
2016-10-19 09:23:55 +0530

    Changes in assignment 3.

--- Assignment 3/Source/svm.py
+++ Assignment 3/Source/svm.py
@@ -16,7 +16,7 @@ def SVM_fit_and_predict(X_train, Y_train, X_test, Y_test, c, kern, gam, plotROC 
 	digits = np.unique(Y_train)
 	multi_Y_train = label_binarize(Y_train, classes = digits)
 	multi_Y_test = label_binarize(Y_test, classes = digits)
-	SVM_K = OneVsRestClassifier(SVC(C = c, max_iter = 1000, kernel = kern, gamma = gam), -1)
+	SVM_K = OneVsRestClassifier(SVC(C = c, max_iter = 1000, kernel = kern, gamma = gam, verbose = True), -1)
 	SVM_K.fit(X_train, multi_Y_train)
 	if plotROC:
 		misc.plot_roc_curve(multi_Y_test, SVM_K.predict(X_test))
@@ -44,7 +44,7 @@ def k_fold_cross_validation(X, Y, k, model, C, kernel, gamma):
 			validation_labels, C, kernel, gamma)[0])
 		start += eff_k
 	mean_accuracy = np.mean(accuracies)
-	print "Cross validation error for (",str(C),",",str(kernel),") :",mean_accuracy
+	print "Cross validation accuracy for (",str(C),",",str(kernel),") :",mean_accuracy
 	return mean_accuracy
 
 
@@ -54,7 +54,7 @@ def grid_search(X, Y, k, model, kernel, grid1, grid2 = ['auto'], plot = False):
 	for gamma in grid2:
 		accuracies = []
 		for C in grid1:
-			accuracy = k_fold_cross_validation(X, Y, k, model, C, kernel, 'auto')
+			accuracy = k_fold_cross_validation(X, Y, k, model, C, kernel, gamma)
 			accuracies.append(accuracy)
 			if accuracy > opt_acc:
 				opt_acc = accuracy
@@ -65,7 +65,7 @@ def grid_search(X, Y, k, model, kernel, grid1, grid2 = ['auto'], plot = False):
 			plt.ylabel(str(k) + '-fold Cross validation accuracy')
 			plt.title('Accuracy v/s C, for gamma = ' + str(gamma))
 			plt.legend(loc="lower right")
-			plt.plot(grid1, accuracies, color='darkorange', lw = 2)
+			plt.plot(grid1, accuracies, color = 'darkorange', lw = 2)
 			plt.show()
 	return opt_val
 
@@ -74,28 +74,32 @@ if __name__ == "__main__":
 	# Process data
 	X_train, Y_train = misc.process_data("MNIST/train-images.idx3-ubyte", "MNIST/train-labels.idx1-ubyte")
 	X_test, Y_test = misc.process_data("MNIST/t10k-images.idx3-ubyte", "MNIST/t10k-labels.idx1-ubyte")
-	C_grid = [1e-10,1e-5,1e0,1e5,1e10]
-	gamma_grid = [1e-5,1e-1,1e0,1e1,1e5]
+	# C_grid = [1e-3,1e-2,1e-1,1e0,1e1]
+	C_grid = [1e-7, 1e-3, 1e1, 1e5]
+	gamma_grid = [1e-9, 1e-6, 1e-3]
+	# gamma_grid = [1e-5,1e-1,1e0,1e1,1e5]
 	train_sampled_X, train_sampled_Y = misc.sample_data(X_train, Y_train, 2000)
 	test_sampled_X, test_sampled_Y = misc.sample_data(X_test, Y_test, 500)
 	# Part(a) : 3/8 binary classification
 	EX, EY = misc.data_for_binary_classification(train_sampled_X, train_sampled_Y, 3, 8)
 	EX_, EY_ = misc.data_for_binary_classification(test_sampled_X, test_sampled_Y, 3, 8)
 	opt_C, opt_gamma = grid_search(EX, EY, 5, SVM_fit_and_predict, 'linear', C_grid)
-	test_accuracy, SVM_OBJ = SVM_fit_and_predict(EX, EY, EX_, EY_, opt_C, 'linear', opt_gamma, True)
+	# test_accuracy, SVM_OBJ = SVM_fit_and_predict(EX, EY, EX_, EY_, opt_C, 'linear', opt_gamma, True)
 	test_accuracy, SVM_OBJ = SVM_fit_and_predict(EX, EY, EX_, EY_, opt_C, 'linear', opt_gamma)
 	print "Test error for (",str(opt_C),",",str(opt_gamma),") :",test_accuracy
 	joblib.dump(SVM_OBJ, "../Models/model_linear.model")
 	# Part(b) : multi-class classification
-	opt_C, opt_gamma = grid_search(train_sampled_X, train_sampled_Y, 5, SVM_fit_and_predict, 'linear', [1e-2, 1e2])
-	# test_accuracy, SVM_OBJ = SVM_fit_and_predict(train_sampled_X, train_sampled_Y, test_sampled_X, test_sampled_Y, opt_C, 'linear', opt_gamma, True)
-	test_accuracy, SVM_OBJ = SVM_fit_and_predict(train_sampled_X, train_sampled_Y, test_sampled_X, test_sampled_Y, opt_C, 'linear', opt_gamma)
+	opt_C, opt_gamma = grid_search(train_sampled_X, train_sampled_Y, 5, SVM_fit_and_predict, 'linear', C_grid)
+	# test_accuracy, SVM_OBJ_2 = SVM_fit_and_predict(train_sampled_X, train_sampled_Y, test_sampled_X, test_sampled_Y, opt_C, 'linear', opt_gamma, True)
+	test_accuracy, SVM_OBJ_2 = SVM_fit_and_predict(train_sampled_X, train_sampled_Y, test_sampled_X, test_sampled_Y, opt_C, 'linear', opt_gamma)
 	print "Test error for (",str(opt_C),",",str(opt_gamma),") :",test_accuracy
-	misc.save_onevsall("../Models/multi", SVM_OBJ)
+	joblib.dump(SVM_OBJ_2, "../Models/multi.model")
+	misc.save_onevsall("../Models/multi", SVM_OBJ_2)
 	# Part(c) : RBF multi-class classification
-	opt_C, opt_gamma = grid_search(train_sampled_X, train_sampled_Y, 5, SVM_fit_and_predict, 'rbf', [1e-2, 1e2], [1e-2, 1e2])
-	# test_accuracy, SVM_OBJ = SVM_fit_and_predict(train_sampled_X, train_sampled_Y, test_sampled_X, test_sampled_Y, opt_C, 'rbf', opt_gamma, True)
-	test_accuracy, SVM_OBJ = SVM_fit_and_predict(train_sampled_X, train_sampled_Y, test_sampled_X, test_sampled_Y, opt_C, 'rbf', opt_gamma)
+	opt_C, opt_gamma = grid_search(train_sampled_X, train_sampled_Y, 5, SVM_fit_and_predict, 'rbf', C_grid, gamma_grid)
+	# test_accuracy, SVM_OBJ_3 = SVM_fit_and_predict(train_sampled_X, train_sampled_Y, test_sampled_X, test_sampled_Y, opt_C, 'rbf', opt_gamma, True)
+	test_accuracy, SVM_OBJ_3 = SVM_fit_and_predict(train_sampled_X, train_sampled_Y, test_sampled_X, test_sampled_Y, opt_C, 'rbf', opt_gamma)
 	print "Test error for (",str(opt_C),",",str(opt_gamma),") :",test_accuracy
-	misc.save_onevsall("../Models/rbf", SVM_OBJ)
+	misc.save_onevsall("../Models/rbf", SVM_OBJ_3)
+	joblib.dump(SVM_OBJ_3, "../Models/rbf.model")
 	
